d83a476a1301041c9cb72dce9627deaef31f3aa6
Simone Basso <bassosimone@gmail.com>
2019-03-16 14:31:20 +0100

    Add working implementation of the collector

--- go/collector/collector.go
+++ go/collector/collector.go
@@ -1,26 +1,129 @@
+// Package collector implements v2.0.0 of the collector protocol.
 package collector
 
-type Settings struct {
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+// Config contains the collector configuration
+type Config struct {
+	// BaseURL is the collector base URL
 	BaseURL string
-	CABundlePath string
-	Timeout int
 }
 
-type OpenRequest struct {
+// Template is the template for opening a report
+type Template struct {
+	// ProbeASN is the probe's autonomous system number
 	ProbeASN string `json:"probe_asn"`
+
+	// ProbeCC is the probe's country code
 	ProbeCC string `json:"probe_cc"`
+
+	// SoftwareName is the app name
 	SoftwareName string `json:"software_name"`
+
+	// SoftwareVersion is the app version
 	SoftwareVersion string `json:"software_version"`
+
+	// TestName is the test name
 	TestName string `json:"test_name"`
+
+	// TestVersion is the test version
 	TestVersion string `json:"test_version"`
 }
 
-type OpenResponse struct {
-	Good bool
-	ReportID string
-	Logs []string
+// Report is an open report
+type Report struct {
+	// ID is the report ID
+	ID string `json:"report_id"`
+
+	// Conf is the configuration used to contact the collector
+	Conf Config
 }
 
-func Open(request OpenRequest) OpenResponse {
-	return OpenResponse{}
+func post(ctx context.Context, c Config, p string, b []byte) ([]byte, error) {
+	URL, err := url.Parse(c.BaseURL)
+	if err != nil {
+		return nil, err
+	}
+	URL.Path = p
+	request, err := http.NewRequest("POST", URL.String(), bytes.NewReader(b))
+	if err != nil {
+		return nil, err
+	}
+	request = request.WithContext(ctx)
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	if response.StatusCode != 200 {
+		return nil, errors.New("The request failed")
+	}
+	defer response.Body.Close()
+	return ioutil.ReadAll(response.Body)
+}
+
+// Open opens a new report with the given context, configuration, and
+// report template. Returns an open report or an error.
+func Open(ctx context.Context, conf Config, rt Template) (Report, error) {
+	report := Report{Conf: conf}
+	data, err := json.Marshal(rt)
+	if err != nil {
+		return report, err
+	}
+	data, err = post(ctx, conf, "/report", data)
+	if err != nil {
+		return report, err
+	}
+	err = json.Unmarshal(data, &report)
+	return report, err
+}
+
+type updateRequest struct {
+	// Format is the data format
+	Format string `json:"format"`
+
+	// Content is the actual report
+	Content interface{} `json:"content"`
+}
+
+type updateResponse struct {
+	// ID is the measurement ID
+	ID string `json:"measurement_id"`
+}
+
+// Update updates a report with the given context and opaque measurement
+// content. Returns the measurement ID or an error.
+func (r Report) Update(ctx context.Context, c interface{}) (string, error) {
+	ureq := updateRequest{
+		Format: "json",
+		Content: c,
+	}
+	data, err := json.Marshal(ureq)
+	if err != nil {
+		return "", err
+	}
+	data, err = post(ctx, r.Conf, fmt.Sprintf("/report/%s", r.ID), data)
+	if err != nil {
+		return "", err
+	}
+	var ures updateResponse
+	err = json.Unmarshal(data, &ures)
+	if err != nil {
+		return "", err
+	}
+	return ures.ID, nil
+}
+
+// Close returns the result of closing an open report.
+func (r Report) Close(ctx context.Context) error {
+	_, err := post(ctx, r.Conf, fmt.Sprintf("/report/%s/close", r.ID), nil)
+	return err
 }
---
+++ go/measurement/measurement.go
@@ -0,0 +1,64 @@
+package measurement
+
+// Measurement contains a measurement result
+type Measurement struct {
+	// Annotations contains results annotations
+	Annotations map[string]string `json:"annotations"`
+
+	// DataFormatVersion is the version of the data format
+	DataFormatVersion string `json:"data_format_version"`
+
+	// ID is the locally generated measurement ID
+	ID string `json:"id"`
+
+	// Input is the measurement input
+	Input string `json:"input"`
+
+	// InputHashes contains input hashes
+	InputHashes []string `json:"input_hashes"`
+
+	// MeasurementStartTime is the time when the measurement started
+	MeasurementStartTime string `json:"measurement_start_time"`
+
+	// Options contains command line options
+	Options []string `json:"options"`
+
+	// ProbeASN contains the probe autonomous system number
+	ProbeASN string `json:"probe_asn"`
+
+	// ProbeCC contains the probe country code
+	ProbeCC string `json:"probe_cc"`
+
+	// ProbeCity contains the probe city
+	ProbeCity string `json:"probe_city"`
+
+	// ProbeIP contains the probe IP
+	ProbeIP string `json:"probe_ip"`
+
+	// ReportID contains the report ID
+	ReportID string `json:"report_id"`
+
+	// SoftwareName contains the software name
+	SoftwareName string `json:"software_name"`
+
+	// SoftwareVersion contains the software version
+	SoftwareVersion string `json:"software_version"`
+
+	// TestHelpers contains the test helpers
+	TestHelpers map[string]string `json:"test_helpers"`
+
+	// TestKeys contains the real test result
+	TestKeys interface{} `json:"test_keys"`
+
+	// TestName contains the test name
+	TestName string `json:"test_name"`
+
+	// TestRuntime contains the test runtime
+	TestRuntime float64 `json:"test_runtime"`
+
+	// TestStartTime contains the test start time
+	TestStartTime string `json:"test_start_time"`
+
+	// TestVersion contains the test version
+	TestVersion string `json:"test_version"`
+}
