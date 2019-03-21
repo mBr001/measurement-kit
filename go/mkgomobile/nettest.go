27160ce5c7f0238b85b0343898abc3e521491e30
Simone Basso <bassosimone@gmail.com>
2019-03-21 16:54:44 +0100

    Remove go/mkgomobile API

--- go/mkgomobile/nettest.go
+++
@@ -1,8 +0,0 @@
-// Package mkgomobile contains the mobile library
-package mkgomobile
-
-// Nettest is the interface of all tests
-type Nettest interface {
-	// Run runs a nettest until it completes
-	Run() bool
-}
